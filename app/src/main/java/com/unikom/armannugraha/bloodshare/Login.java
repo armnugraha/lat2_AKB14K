3df0b4f078f78a1cb5080fd729170411691e0b64
arman nugraha <armannugraha85@gmail.com>
2019-04-18 15:49:03 +0700

    first commit

---
+++ app/src/main/java/com/unikom/armannugraha/bloodshare/Login.java
@@ -0,0 +1,22 @@
+package com.unikom.armannugraha.bloodshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //start hide titlebar & status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_login);
+        //end hide titlebar & status bar
+    }
+}
---
+++ app/src/main/java/com/unikom/armannugraha/bloodshare/UserHome.java
@@ -0,0 +1,22 @@
+package com.unikom.armannugraha.bloodshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class UserHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //start hide titlebar & status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_user_home);
+        //end hide titlebar & status bar
+    }
+}
